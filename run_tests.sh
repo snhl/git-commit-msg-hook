511d761ad987938dae26d9dcac9ed913f77f4544
Sune Hellfritzsch <sunehellfritzsch@gmail.com>
2019-10-02 18:09:12 +0200

    Add return value to test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-echo '- Running negative tests:'
+retval=0
+
+echo '- Running negative tests'
 for f in tests/negative-tests/*.txt
 do
     output=$(./commit-msg $f 2>&1)
-    if [ $? -eq 1 ]; then retval="Passed"; else retval="Failed"; fi
-    echo $f ": " $retval
+    if [ $? -eq 0 ]; then echo $f ": Failed"; retval=1; fi
 done
-echo ''
 
-echo '- Running positive tests:'
+echo '- Running positive tests'
 for f in tests/positive-tests/*.txt
 do
     output=$(./commit-msg $f 2>&1)
-    if [ $? -eq 0 ]; then retval="Passed"; else retval="Failed"; fi
-    echo $f ": " $retval
+    if [ $? -eq 1 ]; then echo $f ": Failed"; retval=1; fi
 done
+
+exit $retval
